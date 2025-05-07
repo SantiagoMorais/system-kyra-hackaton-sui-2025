2f56a1e405eea997cfaa1c9aaec5cc9e546abdb8
Felipe <fillikz@gmail.com>
2025-05-07 20:03:38 -0300

    feat(KyraHome): configure kyra main content

---
+++ src/app/kyra/kyra-home/index.tsx
@@ -0,0 +1,10 @@
+import { KyraChat } from "@/app/components/kyra-chat";
+
+export const KyraHome = () => (
+  <main className="max-w-(--breakpoint-2xl) flex-1">
+    <section></section>
+    <section>
+      <KyraChat />
+    </section>
+  </main>
+);
