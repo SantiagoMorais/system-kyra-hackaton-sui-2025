1b2b24f046d6a4569417b9a4f7802b010ba7faa4
Felipe <fillikz@gmail.com>
2025-05-07 14:07:04 -0300

    refactor(TextArea): refactor component to simplify structure

--- src/app/components/kyra-chat/text-area/index.tsx
+++ src/app/components/kyra-chat/text-area/index.tsx
@@ -1,7 +1,37 @@
-import { TextAreaForm } from "./text-area-form";
+"use client";
 
-export const TextArea = () => (
-  <section className="bg-dark-grey px-3.5 py-4">
-    <TextAreaForm />
-  </section>
-);
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { kyraTextAreaSchema, TKyraTextArea } from "@/core/types/kyra-text-area";
+import { scrollBar } from "@/styles";
+
+import { TextAreaFormFooter } from "./text-area-form-footer";
+
+export const TextArea = () => {
+  const { register, handleSubmit, watch, reset } = useForm<TKyraTextArea>({
+    resolver: zodResolver(kyraTextAreaSchema),
+    defaultValues: { message: "" },
+  });
+
+  const onSubmit = (data: TKyraTextArea) => {
+    console.log(data);
+    reset();
+  };
+
+  return (
+    <section className="bg-dark-grey px-3.5 py-4">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-2">
+        <textarea
+          {...register("message")}
+          className={scrollBar({
+            className:
+              "text-secondary placeholder:text-secondary/60 font-redonda h-30 w-full resize-none outline-0 placeholder:opacity-100",
+          })}
+          placeholder="Ask whatever you need"
+        />
+        <TextAreaFormFooter watch={watch} />
+      </form>{" "}
+    </section>
+  );
+};
