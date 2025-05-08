3f641de7e94984d394875a66e67f5ddb23d8e7dc
Felipe <fillikz@gmail.com>
2025-05-08 09:57:59 -0300

    fix(PanelNavigationItem): ensure the panel item will be selected since the beginning of the component mount

--- src/app/kyra/components/kyra-home/panel-navigator/panel-navbar/panel-navigation-item/index.tsx
+++ src/app/kyra/components/kyra-home/panel-navigator/panel-navbar/panel-navigation-item/index.tsx
@@ -1,14 +1,16 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 import { usePanelNavigator } from "@/contexts/panel-navigator-context/hooks";
 import { IPanelItemProps } from "@/core/interfaces/panel-item-props";
 
 export const PanelNavigationItem = ({ text, setPosition }: IPanelItemProps) => {
-  const { handlePanel } = usePanelNavigator();
+  const { handlePanel, panel } = usePanelNavigator();
   const ref = useRef<HTMLLIElement | null>(null);
+  const [hasInteracted, setHasInteracted] = useState(false);
+
   const handleOnMouseEnter = () => {
     if (!ref.current) return;
-
     const { width, height } = ref.current.getBoundingClientRect();
 
     setPosition({
@@ -20,6 +22,16 @@ export const PanelNavigationItem = ({ text, setPosition }: IPanelItemProps) => {
     });
   };
 
+  const handleClick = () => {
+    setHasInteracted(true);
+    handlePanel(text);
+  };
+
+  const bgClass =
+    !hasInteracted && panel === "wallet" && text === "wallet"
+      ? "bg-orange-600"
+      : "bg-dark-grey";
+
   return (
     <li
       ref={ref}
@@ -27,9 +39,11 @@ export const PanelNavigationItem = ({ text, setPosition }: IPanelItemProps) => {
       key={text}
       className="relative flex-1"
     >
-      <div className="bg-dark-grey absolute z-10 size-full" />
+      <div
+        className={twMerge("bg-dark-grey absolute z-10 size-full", bgClass)}
+      />
       <button
-        onClick={() => handlePanel(text)}
+        onClick={() => handleClick()}
         className={`text-secondary relative z-30 flex w-full justify-center p-3 px-2 text-xs uppercase hover:opacity-70`}
       >
         {text}
