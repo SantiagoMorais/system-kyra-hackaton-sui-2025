ea1a8ea63c14cd8af39a36e075fedabf2ae4acb3
Felipe <fillikz@gmail.com>
2025-05-19 19:09:45 -0300

    fix(ComponentsContainer): ensure KyraNavbar will be on the top of the page, setting KyraHome with flex-1

--- src/app/(private)/chat/components/kyra-home/index.tsx
+++ src/app/(private)/chat/components/kyra-home/index.tsx
@@ -4,7 +4,7 @@ import { DesktopPanel } from "./desktop-panel";
 import { PanelNavigator } from "./panel-navigator";
 
 export const KyraHome = () => (
-  <main className="relative flex h-full w-full flex-col lg:flex-row">
+  <main className="relative flex h-full w-full flex-1 flex-col lg:flex-row">
     <PanelNavigator />
     <DesktopPanel />
     <KyraChat />
